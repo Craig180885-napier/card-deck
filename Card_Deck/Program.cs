e9232b4d1be36c2a9dbd9a0b50abea9b9d8d25c4
Craig <40456374@live.napier.ac.uk>
2021-10-27 21:44:18 +0100

    Add project files.

---
+++ Card_Deck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomNumberGenerator rng = new RandomNumberGenerator(100);
+            Console.WriteLine(rng.getRandomNumber);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Card_Deck/RandomNumberGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Card_Deck
+{
+    class RandomNumberGenerator
+    {
+        private int _randomNumber;
+        
+
+        public RandomNumberGenerator(int range)
+        {
+            // Randomw number code below was sourced from https://stackoverflow.com/questions/3975290/produce-a-random-number-in-a-range-using-c-sharp
+
+            Random r = new Random();
+            _randomNumber = r.Next(0, range);            
+        }
+
+        public int getRandomNumber
+        {
+            get { return _randomNumber; }
+        }
+    }
+}
